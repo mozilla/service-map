f0da9826d5d69448f5b78ab3fa39ccc06a721296
Aaron Meihm <ameihm@mozilla.com>
2016-06-27 13:18:21 -0500

    use riskForRRA in serviceGetRRARisk

--- src/serviceapi/rra.go
+++ src/serviceapi/rra.go
@@ -164,36 +164,17 @@ func serviceGetRRARisk(rw http.ResponseWriter, req *http.Request) {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
-
-	r, err := getRRA(op, rraid)
+	r, err := strconv.Atoi(rraid)
 	if err != nil {
 		op.logf(err.Error())
 		http.Error(rw, err.Error(), 500)
 		return
 	}
-	// Introduce system group metadata into the RRA which datapoints may
-	// use as part of processing.
-	for i := range r.SupportGrps {
-		err = sysGroupAddMeta(op, &r.SupportGrps[i])
-		if err != nil {
-			op.logf(err.Error())
-			http.Error(rw, err.Error(), 500)
-			return
-		}
-	}
-
-	rs := slib.RRAServiceRisk{}
-	rs.RRA = r
-	err = riskCalculation(op, &rs)
+	rs, err := riskForRRA(op, r)
 	if err != nil {
 		op.logf(err.Error())
 		http.Error(rw, err.Error(), 500)
 		return
-	}
-	err = rs.Validate()
-	if err != nil {
-		op.logf(err.Error())
-		http.Error(rw, err.Error(), 500)
 	}
 
 	buf, err := json.Marshal(&rs)
