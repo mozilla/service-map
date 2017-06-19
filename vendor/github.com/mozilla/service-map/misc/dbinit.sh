91509318f2ac20b3e5e3237d7a716262be2fe81b
Aaron Meihm <ameihm@mozilla.com>
2017-06-19 16:24:37 -0500

    vendor current deps

---
+++ vendor/github.com/mozilla/service-map/misc/dbinit.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+dbname=servicemap
+psql="psql -f - ${dbname}"
+
+$psql << EOF
+DROP TABLE IF EXISTS importcomphostcfg;
+DROP TABLE IF EXISTS vulnscore;
+DROP TABLE IF EXISTS vulnstatus;
+DROP TABLE IF EXISTS migstatus;
+DROP TABLE IF EXISTS compscore;
+DROP TABLE IF EXISTS httpobsscore;
+DROP TABLE IF EXISTS rra_sysgroup;
+DROP TABLE IF EXISTS asset;
+DROP TABLE IF EXISTS assetawsmeta;
+DROP TABLE IF EXISTS assetowners;
+DROP TABLE IF EXISTS risk;
+DROP TABLE IF EXISTS rra;
+DROP TABLE IF EXISTS sysgroup;
+DROP TABLE IF EXISTS searchresults;
+DROP TABLE IF EXISTS interlinks;
+CREATE TABLE rra (
+	rraid SERIAL PRIMARY KEY,
+	service TEXT NOT NULL,
+	ari TEXT NOT NULL,
+	api TEXT NOT NULL,
+	afi TEXT NOT NULL,
+	cri TEXT NOT NULL,
+	cpi TEXT NOT NULL,
+	cfi TEXT NOT NULL,
+	iri TEXT NOT NULL,
+	ipi TEXT NOT NULL,
+	ifi TEXT NOT NULL,
+	arp TEXT NOT NULL,
+	app TEXT NOT NULL,
+	afp TEXT NOT NULL,
+	crp TEXT NOT NULL,
+	cpp TEXT NOT NULL,
+	cfp TEXT NOT NULL,
+	irp TEXT NOT NULL,
+	ipp TEXT NOT NULL,
+	ifp TEXT NOT NULL,
+	datadefault TEXT NOT NULL,
+	lastupdated TIMESTAMP WITH TIME ZONE NOT NULL,
+	lastmodified TIMESTAMP WITH TIME ZONE NOT NULL,
+	raw JSON NOT NULL,
+	UNIQUE(service, lastmodified)
+);
+CREATE TABLE risk (
+	rraid INTEGER REFERENCES rra (rraid),
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	risk JSON NOT NULL
+);
+CREATE INDEX ON risk (rraid);
+CREATE INDEX ON risk (timestamp);
+CREATE TABLE sysgroup (
+	sysgroupid SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	UNIQUE(name)
+);
+CREATE TABLE rra_sysgroup (
+	rraid INTEGER REFERENCES rra (rraid),
+	sysgroupid INTEGER REFERENCES sysgroup (sysgroupid),
+	UNIQUE(rraid, sysgroupid)
+);
+CREATE TABLE assetowners (
+	ownerid SERIAL PRIMARY KEY,
+	team TEXT NOT NULL,
+	operator TEXT NOT NULL,
+	UNIQUE (team, operator)
+);
+CREATE TABLE assetawsmeta (
+	assetawsmetaid SERIAL PRIMARY KEY,
+	accountid TEXT NOT NULL,
+	accountname TEXT NOT NULL,
+	region TEXT NOT NULL,
+	instancetype TEXT NOT NULL,
+	instanceid TEXT NOT NULL,
+	public_ip INET,
+	private_ip INET,
+	private_dns TEXT,
+	public_dns TEXT,
+	tags JSON,
+	lastupdated TIMESTAMP WITH TIME ZONE NOT NULL
+);
+CREATE TABLE asset (
+	assetid SERIAL PRIMARY KEY,
+	assettype TEXT NOT NULL,
+	hostname TEXT,
+	website TEXT,
+	sysgroupid INTEGER REFERENCES sysgroup (sysgroupid),
+	ownerid INTEGER REFERENCES assetowners (ownerid),
+	assetawsmetaid INTEGER REFERENCES assetawsmeta (assetawsmetaid),
+	v2boverride TEXT,
+	comment TEXT,
+	dynamic BOOLEAN NOT NULL,
+	dynamic_added TIMESTAMP WITH TIME ZONE,
+	dynamic_confidence INTEGER,
+	lastused TIMESTAMP WITH TIME ZONE NOT NULL,
+	lastcompscore TIMESTAMP WITH TIME ZONE NOT NULL,
+	lastvulnscore TIMESTAMP WITH TIME ZONE NOT NULL,
+	lasthttpobsscore TIMESTAMP WITH TIME ZONE NOT NULL,
+	UNIQUE(assettype, hostname)
+);
+CREATE INDEX ON asset (hostname);
+CREATE INDEX ON asset (sysgroupid);
+CREATE TABLE searchresults (
+	opid TEXT NOT NULL,
+	identifier TEXT NOT NULL,
+	result TEXT NOT NULL,
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	UNIQUE(opid, identifier)
+);
+CREATE TABLE compscore (
+	scoreid SERIAL PRIMARY KEY,
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	assetid INTEGER REFERENCES asset (assetid),
+	checkref TEXT NOT NULL,
+	status BOOLEAN NOT NULL
+);
+CREATE INDEX ON compscore (assetid, timestamp DESC);
+CREATE TABLE vulnscore (
+	scoreid SERIAL PRIMARY KEY,
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	assetid INTEGER REFERENCES asset (assetid),
+	maxcount INTEGER DEFAULT 0 NOT NULL,
+	highcount INTEGER DEFAULT 0 NOT NULL,
+	mediumcount INTEGER DEFAULT 0 NOT NULL,
+	lowcount INTEGER DEFAULT 0 NOT NULL,
+	likelihoodindicator INTEGER DEFAULT 0 NOT NULL
+);
+CREATE INDEX ON vulnscore (assetid, timestamp DESC);
+CREATE TABLE httpobsscore (
+	scoreid SERIAL PRIMARY KEY,
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	assetid INTEGER REFERENCES asset (assetid),
+	score INTEGER NOT NULL,
+	grade TEXT NOT NULL,
+	passcount INTEGER NOT NULL,
+	failcount INTEGER NOT NULL,
+	totalcount INTEGER NOT NULL
+);
+CREATE INDEX ON httpobsscore (assetid);
+CREATE TABLE vulnstatus (
+	statusid SERIAL PRIMARY KEY,
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	assetid INTEGER REFERENCES asset (assetid),
+	checktype TEXT NOT NULL,
+	status BOOLEAN NOT NULL
+);
+CREATE INDEX ON vulnstatus (assetid);
+CREATE TABLE migstatus (
+	statusid SERIAL PRIMARY KEY,
+	timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+	assetid INTEGER REFERENCES asset (assetid),
+	version TEXT NOT NULL,
+	env JSON,
+	tags JSON
+);
+CREATE TABLE importcomphostcfg (
+	exid SERIAL PRIMARY KEY,
+	hostmatch TEXT NOT NULL UNIQUE
+);
+CREATE TABLE interlinks (
+	ruleid SERIAL PRIMARY KEY,
+	ruletype INTEGER NOT NULL,
+	srchostmatch TEXT,
+	srcawssqlmatch TEXT,
+	srcsysgroupmatch TEXT,
+	destsysgroupmatch TEXT,
+	destservicematch TEXT,
+	srcwebsitematch TEXT,
+	destwebsitematch TEXT,
+	destoperatormatch TEXT,
+	destteammatch TEXT,
+	destv2boverride TEXT
+);
+EOF
+
+exit 0
---
+++ vendor/github.com/mozilla/service-map/src/servicelib/rra.go
@@ -0,0 +1,311 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Contributor:
+// - Aaron Meihm ameihm@mozilla.com
+
+package servicelib
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// Describes an RRA.
+type RRAService struct {
+	Name string `json:"name,omitempty"`
+	ID   int    `json:"id,omitempty"`
+
+	/* Attribute impact scores */
+	AvailRepImpact string `json:"availability_reputation_impact,omitempty"`
+	AvailPrdImpact string `json:"availability_productivity_impact,omitempty"`
+	AvailFinImpact string `json:"availability_financial_impact,omitempty"`
+	IntegRepImpact string `json:"integrity_reputation_impact,omitempty"`
+	IntegPrdImpact string `json:"integrity_productivity_impact,omitempty"`
+	IntegFinImpact string `json:"integrity_financial_impact,omitempty"`
+	ConfiRepImpact string `json:"confidentiality_reputation_impact,omitempty"`
+	ConfiPrdImpact string `json:"confidentiality_productivity_impact,omitempty"`
+	ConfiFinImpact string `json:"confidentiality_financial_impact,omitempty"`
+
+	/* Attribute probability scores */
+	AvailRepProb string `json:"availability_reputation_probability,omitempty"`
+	AvailPrdProb string `json:"availability_productivity_probability,omitempty"`
+	AvailFinProb string `json:"availability_financial_probability,omitempty"`
+	IntegRepProb string `json:"integrity_reputation_probability,omitempty"`
+	IntegPrdProb string `json:"integrity_productivity_probability,omitempty"`
+	IntegFinProb string `json:"integrity_financial_probability,omitempty"`
+	ConfiRepProb string `json:"confidentiality_reputation_probability,omitempty"`
+	ConfiPrdProb string `json:"confidentiality_productivity_probability,omitempty"`
+	ConfiFinProb string `json:"confidentiality_financial_probability,omitempty"`
+
+	DefData string `json:"default_data_classification,omitempty"`
+
+	// Supporting system groups
+	SupportGrps []SystemGroup `json:"supporting_system_groups,omitempty"`
+
+	RawRRA json.RawMessage `json:"rra_details,omitempty"` // The raw RRA as described in ES
+}
+
+func (r *RRAService) Validate() error {
+	if r.Name == "" {
+		return fmt.Errorf("rra must have a name")
+	}
+	return nil
+}
+
+func (r *RRAService) HighestRiskReputation() (float64, float64) {
+	// XXX Assumed values have been normalized here
+	repavi, _ := ImpactValueFromLabel(r.AvailRepImpact)
+	repavp, _ := ImpactValueFromLabel(r.AvailRepProb)
+	repcfi, _ := ImpactValueFromLabel(r.ConfiRepImpact)
+	repcfp, _ := ImpactValueFromLabel(r.ConfiRepProb)
+	repiti, _ := ImpactValueFromLabel(r.IntegRepImpact)
+	repitp, _ := ImpactValueFromLabel(r.IntegRepProb)
+	rskav := repavi * repavp
+	rskcf := repcfi * repcfp
+	rskit := repiti * repitp
+	var candr, candi, candp *float64
+	candi = &repavi
+	candp = &repavp
+	candr = &rskav
+	if rskcf > *candr {
+		candi = &repcfi
+		candp = &repcfp
+		candr = &rskcf
+	}
+	if rskit > *candr {
+		candi = &repiti
+		candp = &repitp
+		candr = &rskit
+	}
+	return *candi, *candp
+}
+
+func (r *RRAService) HighestRiskProductivity() (float64, float64) {
+	// XXX Assumed values have been normalized here
+	prdavi, _ := ImpactValueFromLabel(r.AvailPrdImpact)
+	prdavp, _ := ImpactValueFromLabel(r.AvailPrdProb)
+	prdcfi, _ := ImpactValueFromLabel(r.ConfiPrdImpact)
+	prdcfp, _ := ImpactValueFromLabel(r.ConfiPrdProb)
+	prditi, _ := ImpactValueFromLabel(r.IntegPrdImpact)
+	prditp, _ := ImpactValueFromLabel(r.IntegPrdProb)
+	rskav := prdavi * prdavp
+	rskcf := prdcfi * prdcfp
+	rskit := prditi * prditp
+	var candr, candi, candp *float64
+	candi = &prdavi
+	candp = &prdavp
+	candr = &rskav
+	if rskcf > *candr {
+		candi = &prdcfi
+		candp = &prdcfp
+		candr = &rskcf
+	}
+	if rskit > *candr {
+		candi = &prditi
+		candp = &prditp
+		candr = &rskit
+	}
+	return *candi, *candp
+}
+
+func (r *RRAService) HighestRiskFinancial() (float64, float64) {
+	// XXX Assumed values have been normalized here
+	finavi, _ := ImpactValueFromLabel(r.AvailFinImpact)
+	finavp, _ := ImpactValueFromLabel(r.AvailFinProb)
+	fincfi, _ := ImpactValueFromLabel(r.ConfiFinImpact)
+	fincfp, _ := ImpactValueFromLabel(r.ConfiFinProb)
+	finiti, _ := ImpactValueFromLabel(r.IntegFinImpact)
+	finitp, _ := ImpactValueFromLabel(r.IntegFinProb)
+	rskav := finavi * finavp
+	rskcf := fincfi * fincfp
+	rskit := finiti * finitp
+	var candr, candi, candp *float64
+	candi = &finavi
+	candp = &finavp
+	candr = &rskav
+	if rskcf > *candr {
+		candi = &fincfi
+		candp = &fincfp
+		candr = &rskcf
+	}
+	if rskit > *candr {
+		candi = &finiti
+		candp = &finitp
+		candr = &rskit
+	}
+	return *candi, *candp
+}
+
+// Score values per impact label
+const (
+	ImpactUnknownValue = 0.0
+	ImpactLowValue     = 1.0
+	ImpactMediumValue  = 2.0
+	ImpactHighValue    = 3.0
+	ImpactMaxValue     = 4.0
+)
+
+// Values for data classification
+const (
+	DataUnknownValue = 0.0
+	DataPublicValue  = 1.0
+	DataConfIntValue = 2.0
+	DataConfResValue = 3.0
+	DataConfSecValue = 4.0
+)
+
+type RRAAttribute struct {
+	Attribute   string  `json:"attribute"`
+	Impact      float64 `json:"impact"`
+	Probability float64 `json:"probability"`
+}
+
+// Describes calculated risk for a service, based on an RRA and known
+// data points
+type RRAServiceRisk struct {
+	RRA RRAService `json:"rra"` // The RRA we are describing
+
+	// The attribute from the RRA we use as the basis for risk calculations
+	// (business impact) for the service. For example, this could be "reputation",
+	// "productivity", or "financial" depending on which attribute in the RRA
+	// yields the highest defined risk.
+	//
+	// Previous versions of this would generate scenarios across all attributes
+	// in the RRA. This behavior is not necessarily desirable as it can end up
+	// devaluing high impact attributes when we consider the entire set combined.
+	UsedRRAAttrib RRAAttribute
+
+	Risk struct {
+		WorstCase      float64 `json:"worst_case"`
+		WorstCaseLabel string  `json:"worst_case_label"`
+		Median         float64 `json:"median"`
+		MedianLabel    string  `json:"median_label"`
+		Average        float64 `json:"average"`
+		AverageLabel   string  `json:"average_label"`
+		DataClass      float64 `json:"data_classification"`
+		Impact         float64 `json:"highest_business_impact"`
+		ImpactLabel    string  `json:"highest_business_impact_label"`
+	} `json:"risk"`
+
+	Scenarios []RiskScenario `json:"scenarios"` // Risk scenarios
+}
+
+func (r *RRAServiceRisk) Validate() error {
+	err := r.RRA.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Stores information used to support probability for risk calculation; this
+// generally would be created using control information and is combined with the
+// RRA impact scores to produce estimated service risk
+type RiskScenario struct {
+	Name        string  `json:"name"` // Name describing the datapoint
+	Probability float64 `json:"probability"`
+	Impact      float64 `json:"impact"`
+	Score       float64 `json:"score"`
+	NoData      bool    `json:"nodata"`   // No data exists for proper calculation
+	Coverage    string  `json:"coverage"` // Coverage (partial, complete, none, unknown)
+}
+
+// Validates a RiskScenario for consistency
+func (r *RiskScenario) Validate() error {
+	if r.Name == "" {
+		return fmt.Errorf("scenario must have a name")
+	}
+	if r.Coverage != "none" && r.Coverage != "partial" &&
+		r.Coverage != "complete" && r.Coverage != "unknown" {
+		return fmt.Errorf("scenario \"%v\" coverage invalid \"%v\"", r.Name, r.Coverage)
+	}
+	return nil
+}
+
+// Convert an impact label into the numeric representation from 1 - 4 for
+// that label
+func ImpactValueFromLabel(l string) (float64, error) {
+	switch l {
+	case "maximum":
+		return ImpactMaxValue, nil
+	case "high":
+		return ImpactHighValue, nil
+	case "medium":
+		return ImpactMediumValue, nil
+	case "low":
+		return ImpactLowValue, nil
+	case "unknown":
+		// XXX Return low here if the value is set to unknown to handle older
+		// format RRAs and still use the data in risk calculation; returning
+		// valid data here is important for riskFindHighestImpact()
+		return ImpactLowValue, nil
+	}
+	return 0, fmt.Errorf("invalid impact label %v", l)
+}
+
+// Convert an impact label into the numeric representation from 1 - 4 for
+// that label
+func DataValueFromLabel(l string) (float64, error) {
+	switch l {
+	case "confidential secret":
+		return DataConfSecValue, nil
+	case "confidential restricted":
+		return DataConfResValue, nil
+	case "confidential internal":
+		return DataConfIntValue, nil
+	case "public":
+		return DataPublicValue, nil
+	case "unknown":
+		return DataUnknownValue, nil
+	}
+	return 0, fmt.Errorf("invalid impact label %v", l)
+}
+
+// Sanitize an impact label and verify it's a valid value
+func SanitizeImpactLabel(l string) (ret string, err error) {
+	if l == "" {
+		err = fmt.Errorf("invalid zero length label")
+		return
+	}
+	ret = strings.ToLower(l)
+	if ret != "maximum" && ret != "high" && ret != "medium" &&
+		ret != "low" && ret != "unknown" {
+		err = fmt.Errorf("invalid impact label \"%v\"", ret)
+	}
+	return
+}
+
+// Covert an impact value from 1 - 4 to the string value for that label,
+// note that does not handle decimal values in the floating point value
+// and should only be used with 1.0, 2.0, 3.0, or 4.0
+func ImpactLabelFromValue(v float64) (string, error) {
+	switch v {
+	case ImpactMaxValue:
+		return "maximum", nil
+	case ImpactHighValue:
+		return "high", nil
+	case ImpactMediumValue:
+		return "medium", nil
+	case ImpactLowValue:
+		return "low", nil
+	case ImpactUnknownValue:
+		return "unknown", nil
+	}
+	return "", fmt.Errorf("invalid impact value %v", v)
+}
+
+// Given a risk score from 1 - 16, convert that sore into
+// the string value that represents the risk
+func NormalLabelFromValue(v float64) string {
+	if v >= 13 {
+		return "maximum"
+	} else if v >= 9 {
+		return "high"
+	} else if v >= 5 {
+		return "medium"
+	}
+	return "low"
+}
