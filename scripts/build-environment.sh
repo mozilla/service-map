92f10b489d5abc16e64950aa7779644fe5e38a05
Aaron Meihm <ameihm@mozilla.com>
2017-06-22 16:17:26 -0500

    log build

--- scripts/build-environment.sh
+++ scripts/build-environment.sh
@@ -2,4 +2,4 @@
 
 source /etc/serviceapiprofile
 cd /home/serviceapi/app/ansible
-/usr/local/bin/ansible-container build
+/usr/local/bin/ansible-container --debug build >/home/serviceapi/build.log 2>&1
