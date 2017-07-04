2b08e1de06c3177005d036a460a60f4cccf29a60
Aaron Meihm <ameihm@mozilla.com>
2017-07-04 13:01:05 -0500

    move app environment creation into post deploy phase

--- scripts/prep-deploy.sh
+++ scripts/prep-deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Create a fresh application environment
-touch /etc/serviceapienv
-chmod 600 /etc/serviceapienv
-bash /home/serviceapi/app/scripts/appenv.sh > /etc/serviceapienv
-if [[ $? -ne 0 ]]; then
-	exit 1
-fi
-
 if [[ -f /home/serviceapi/app/Dockerfile ]]; then
 	docker rm $(docker stop $(docker ps -a -q --filter ancestor=serviceapi:latest))
 fi
