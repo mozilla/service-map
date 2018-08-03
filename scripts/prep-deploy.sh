8108f6a6063568df1aff4912dd0aa3ca6741776d
Jeff Bryner <jeff@jeffbryner.com>
2018-08-03 12:11:13 -0700

    rm scripts

--- scripts/prep-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -f /home/serviceapi/app/Dockerfile ]]; then
-	docker rm $(docker stop $(docker ps -a -q --filter ancestor=serviceapi:latest))
-fi
-
-rm -rf /home/serviceapi/app
-mkdir -p /home/serviceapi/app
