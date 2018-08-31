6057487ab38819e9d375b5b019d93e17296c0c83
Jeff Bryner <jeff@jeffbryner.com>
2018-08-31 13:32:50 -0700

    add asset api

--- models/v1/assets/asset.py
+++ models/v1/assets/asset.py
@@ -35,3 +35,32 @@ class Asset(DynaModel):
         zone = String(required=True)
         timestamp_utc = String(default=datetime.now(timezone.utc).isoformat())
         description = String()
+
+@api.route("/status")
+class status(Resource):
+    @api.doc('a klingon test/status endpoint')
+    def get(self):
+        body = {
+            "message": "Qapla'!"
+        }
+        return jsonify(body)
+
+@api.route("s/<identifier>")
+@api.route("s/",defaults={'identifier':None})
+class search(Resource):
+    @api.doc("/<asset_identifier> partial or full asset identifier to return all matches for this word/term")
+    def get(self, identifier):
+        try:
+            assets=[]
+
+            if identifier is not None:
+                for asset in Asset.scan(asset_identifier__contains=identifier):
+                    assets.append(asset.to_dict())
+            else:
+                for asset in Asset.scan(asset_identifier__exists=True):
+                    assets.append(asset.to_dict())
+
+            return json.dumps(assets),200
+        except Exception as e:
+            message = {"exception": "{}".format(e)}
+            return json.dumps(message),500
