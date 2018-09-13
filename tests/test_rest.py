0a560227251fd083cea9d3a5552544bef444b1aa
Jeff Bryner <jeff@jeffbryner.com>
2018-09-13 08:38:26 -0700

    move tests to classes

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -6,24 +6,27 @@ API_URL = os.environ.get("API_URL")
 if not API_URL.endswith("/"):
     API_URL= API_URL+ "/"
 
-def test_api_status():
-    r=requests.get('{}status'.format(API_URL))
-    assert r.json()['message'] == "Qapla'!"
+class TestStatus(object):
+    def test_api_status(self):
+        r=requests.get('{}status'.format(API_URL))
+        assert r.json()['message'] == "Qapla'!"
 
-def test_asset_status():
-    r=requests.get('{}api/v1/asset/status'.format(API_URL))
-    assert r.json()['message'] == "Qapla'!"
+    def test_asset_status(self):
+        r=requests.get('{}api/v1/asset/status'.format(API_URL))
+        assert r.json()['message'] == "Qapla'!"
 
-def test_indicator_status():
-    r=requests.get('{}api/v1/indicator/status'.format(API_URL))
-    assert r.json()['message'] == "Qapla'!"
+    def test_indicator_status(self):
+        r=requests.get('{}api/v1/indicator/status'.format(API_URL))
+        assert r.json()['message'] == "Qapla'!"
 
-def test_nonexistent_asset():
-    r=requests.get('{}api/v1/assets/hereisathingthatshouldnotexist'.format(API_URL))
-    result=json.loads(r.json())
-    assert len(result)==0
+class TestMissing(object):
+    def test_nonexistent_asset(self):
+        r=requests.get('{}api/v1/assets/hereisathingthatshouldnotexist'.format(API_URL))
+        result=json.loads(r.json())
+        assert len(result)==0
 
-def test_nonexistent_indicator():
-    r=requests.get('{}api/v1/indicators/hereisathingthatshouldnotexist'.format(API_URL))
-    result=json.loads(r.json())
-    assert len(result)==0
+    def test_nonexistent_indicator(self):
+        r=requests.get('{}api/v1/indicators/hereisathingthatshouldnotexist'.format(API_URL))
+        result=json.loads(r.json())
+        assert len(result)==0
+
