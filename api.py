6ca71ffb984750941c7f7529d19cb53865e350c0
Jeff Bryner <jeff@jeffbryner.com>
2018-09-17 10:55:12 -0700

    add asset groups

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from flask_cors import cross_origin
 from flask_restplus import Resource,Api
 from models.v1.indicators.indicator import api as indicator_api
 from models.v1.assets.asset import api as asset_api
+from models.v1.asset_groups import asset_group import api as asset_group_api
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,7 @@ app = Flask(__name__)
 api = Api(app)
 api.add_namespace(indicator_api)
 api.add_namespace(asset_api)
+api.add_namespace(asset_group_api)
 
 @api.route('/status')
 class status(Resource):
