f108db02c515981cb6a5e632031b33ce25888c72
Jeff Bryner <jeff@jeffbryner.com>
2018-09-17 15:34:08 -0700

    rework rule format, remove host/website distinction

--- bucket.py
+++ bucket.py
@@ -1,5 +1,6 @@
 import boto3
 from models.v1.assets.asset import Asset
+from models.v1.asset_groups.asset_group import AssetGroup
 
 class iRule():
     def __init__(self,ruletype,action,tokens=[]):
@@ -34,8 +35,7 @@ def event(event, context):
         print(record['s3']['object']['key'])
         bucket = record['s3']['bucket']['name']
         key=record['s3']['object']['key']
-        #response= s3.get_object(Bucket=bucket, Key=key)
-        #contents=response['Body'].read().decode('utf-8')
+        #download as a file for easier line by line parsing
         s3.meta.client.download_file(bucket, key, '/tmp/{}'.format(key))
         #parsing the interlink.rules
         rules=[]
@@ -50,9 +50,9 @@ def event(event, context):
             if len(tokens)<2 or '#' in tokens[0]:
                 continue
 
-            if len(tokens) == 3 and tokens[0] in ["add","remove"] and tokens[1] == "assetgroup":
+            if len(tokens) > 2 and tokens[0] in ["add","remove"] and tokens[1] == "assetgroup":
                 #add/remove an asset group
-                #ex: add assetgroup mana-production
+                #ex: add assetgroup mana-production description goes here
                 rules.append(iRule('assetgroup',tokens[0],tokens))
 
             elif (len(tokens) == 6 and
@@ -65,42 +65,23 @@ def event(event, context):
                 rules.append(iRule('assetgroupLinkService','link',tokens))
 
             elif (len(tokens) == 6 and
-                tokens[0] == "host" and
+                tokens[0] == "asset" and
                 tokens[1] == "matches" and
                 tokens[3] == "link" and
                 tokens[4] == "assetgroup"):
-                #link a host to an asset group
-                #ex: host matches ^mana\d\.db\..* link assetgroup mana-production
-                rules.append(iRule('hostLinkAssetgroup','link',tokens))
-
-            elif (len(tokens) == 6 and
-                tokens[0] == "website" and
-                tokens[1] == "matches" and
-                tokens[3] == "link" and
-                tokens[4] == "assetgroup"):
-                #link a website to an asset group
-                #ex: website matches mana\.mozilla\.org link assetgroup mana-production
-                rules.append(iRule('websiteLinkAssetgroup','link',tokens))
+                #link an asset to an asset group
+                #ex: asset matches mana link assetgroup mana-production
+                rules.append(iRule('assetLinkAssetgroup','link',tokens))
 
             elif (len(tokens) >= 6 and
-                tokens[0] == "host" and
+                tokens[0] == "asset" and
                 tokens[1] == "matches" and
                 tokens[3] == "ownership" ):
-                #link a host to an owner
-                #ex: host matches mana1\.webapp\.scl3\.mozilla\.com ownership it webops webops-special
+                #link an asset to an owner
+                #ex: asset matches mana1\.webapp\.scl3\.mozilla\.com ownership it webops webops-special
                 #remove simple regex escapes
                 tokens[2]=tokens[2].replace('\\d+','').replace('\\d','').replace('\\','')
-                rules.append(iRule('hostOwnership','link',tokens))
-
-            elif len(tokens) == 4 and tokens[0] in ["add","remove"] and tokens[1] == "owner":
-                #add/remove an owner
-                #ex: add owner it webops
-                rules.append(iRule('owner',tokens[0],tokens))
-
-            elif len(tokens) == 3 and tokens[0] in ["add","remove"] and tokens[1] == "website":
-                #add/remove a website
-                #ex: add website mana.mozilla.org
-                rules.append(iRule('website',tokens[0],tokens))
+                rules.append(iRule('assetOwnership','link',tokens))
 
             elif (len(tokens) == 4 and
                 tokens[0] == "service" and
@@ -113,13 +94,44 @@ def event(event, context):
 
 
         for rule in rules:
+            #debug
             print(rule.ruletype,
                 rule.action,
                 rule.tokens)
-            if rule.ruletype == 'hostOwnership':
+            if rule.ruletype == 'assetgroup':
+                if rule.tokens[0] == 'add':
+                    # add unless it already exists
+                    asset_group=None
+                    asset_groups=[a for a in AssetGroup.scan(name__eq=rule.tokens[2])]
+                    if len(asset_groups):
+                        asset_group=asset_groups[0]
+                    else:
+                        asset_group = AssetGroup(name=rule.tokens[2])
+                    # add any description
+                    if len(rule.tokens)>3:
+                        asset_group.description = ' '.join(rule.tokens[3::])
+                    asset_group.save()
+
+
+            if rule.ruletype == 'assetOwnership':
                 for asset in Asset.scan(asset_identifier__contains=rule.tokens[2]):
                     print('updating: {}'.format(asset.asset_identifier))
                     asset.team=rule.tokens[4]
                     asset.operator=rule.tokens[5]
                     asset.save()
+
+            if rule.ruletype == 'assetLinkAssetgroup':
+                asset_group = rule.tokens[-1]
+                asset_group_id = None
+                asset_identifier = rule.tokens[2]
+                asset_id = None
+                # get the group id
+                for group in AssetGroup.scan(name__eq=asset_group):
+                    asset_group_id= group.id
+                # update all assets to be in this group
+                for asset in Asset.scan(asset_identifier__contains=asset_identifier):
+                    asset.asset_group_id= asset_group_id
+                    asset.save()
+
+
 
