04b8132430192eac659f2be3d542ca43275f5371
Jeff Bryner <jeff@jeffbryner.com>
2018-09-26 11:47:15 -0700

    remove credstash

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,3 @@
-import credstash
 import os
 import uuid
 import yaml
@@ -15,20 +14,6 @@ def get_config():
     if conf is None:
         conf=dict()
 
-    # get our secrets:
-    creds= {
-        'OIDC_CLIENT_ID': credstash.getSecret(
-            name="serviceapi.oidc_client_id",
-            context={'app': 'serviceapi'},
-            region="us-east-1"
-            ),
-        'OIDC_CLIENT_SECRET': credstash.getSecret(
-            name="serviceapi.oidc_client_secret",
-            context={'app': 'serviceapi'},
-            region="us-east-1"
-            ),
-    }
-    os.environ.update(creds)
     return ConfigManager(
         [
             ConfigOSEnv(),
